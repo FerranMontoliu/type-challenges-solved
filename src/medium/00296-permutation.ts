68d2800c5e85f06a9e163c08c7a1adc8aaa7a22f
Ferran Montoliu <montoliuferran@gmail.com>
2024-09-01 14:06:47 +0200

    feat: 296 | Permutation.

---
+++ src/medium/00296-permutation.ts
@@ -0,0 +1,48 @@
+/*
+  296 - Permutation
+  -------
+  by Naoto Ikuno (@pandanoir) #medium #union
+
+  ### Question
+
+  Implement permutation type that transforms union types into the array that includes permutations of unions.
+
+  ```typescript
+  type perm = Permutation<'A' | 'B' | 'C'>; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+  ```
+
+  > View on GitHub: https://tsch.js.org/296
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type IsNever<T> =
+    [T] extends [never] // Checking `T extends never` does not do the trick, so this is needed instead
+        ? true
+        : false
+
+// Highly inspired from the awesome answer found in https://github.com/type-challenges/type-challenges/issues/614
+type Permutation<Union, Item=Union> = // Item=Union needs to be done in order to do the foreach below
+    IsNever<Union> extends true
+        ? []
+        : Item extends Item // For each Item in Union
+            ? [Item, ...Permutation<Exclude<Union, Item>>] // Recursive call
+            : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+    Expect<Equal<Permutation<'A'>, ['A']>>,
+    Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+    Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+    Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+    Expect<Equal<Permutation<never>, []>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/296/answer
+  > View solutions: https://tsch.js.org/296/solutions
+  > More Challenges: https://tsch.js.org
+*/
